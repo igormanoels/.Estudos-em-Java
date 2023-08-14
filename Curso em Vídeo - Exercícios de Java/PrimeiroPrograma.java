4258882a919ce822ba5c3be0377ce1df328e049a
igormanoels <s.igormanoel@gmail.com>
2023-08-14 12:24:25 -0300

    Me livrando da maldição

---
+++ Curso em Vídeo - Exercícios de Java/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package com.mycompany.primeiroprograma;
+
+/**
+ *
+ * @author igor_manoel
+ */
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # .Fatec_Exercicios_Java
 Exercícios realizados no curso de Análise e Desenvolvimento de Sistemas, Disciplina de Linguagem de Programação.
+
+## Listas de Exercícios
+
+### Lista 1
