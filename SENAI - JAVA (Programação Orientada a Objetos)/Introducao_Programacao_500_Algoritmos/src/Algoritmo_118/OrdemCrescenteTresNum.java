1e80a64c09ad3c0f8118a8fec76224a1cf63166f
igormanoels <s.igormanoel@gmail.com>
2023-12-30 21:52:09 -0300

    Exercício 118

---
+++ SENAI - JAVA (Programação Orientada a Objetos)/Introducao_Programacao_500_Algoritmos/src/Algoritmo_118/OrdemCrescenteTresNum.java
@@ -0,0 +1,50 @@
+package Algoritmo_118;
+
+import java.util.Scanner;
+
+public class OrdemCrescenteTresNum {
+		static int num1, num2, num3;
+		
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		System.out.println("Algoritmo: Lê três números e imprimi em ordem crescente.");
+		
+		System.out.print("Informe o primeiro número: ");
+			num1 = ler.nextInt();
+		System.out.print("Informe o segundo número: ");
+			num2 = ler.nextInt();
+		System.out.print("Informe o terceiro número: ");
+			num3 = ler.nextInt();
+/*
+ * sequencia de eventos para teste de mesa
+ * 1 2 3 
+ * 1 3 2	
+ * 2 1 3
+ * 2 3 1
+ * 3 1 2
+ * 3 2 1
+ */
+		if (num1 < num3 && num1 < num2 && num2 < num3) {
+			System.out.println("Ordem: " + num1 + ", " + num2 + ", " + num3 + ".");
+		}
+			else if (num1 < num3 && num1 < num2 && num3 < num2) {
+				System.out.println("Ordem: " + num1 + ", " + num3 + ", " + num2 + ".");
+			}
+			else if (num2 < num3 && num2 < num1 && num1 < num3) {
+				System.out.println("Ordem: " + num2 + ", " + num1 + ", " + num3 + ".");
+			}
+			else if (num2 < num3 && num2 < num1 && num3 < num1) {
+				System.out.println("Ordem: " + num2 + ", " + num3 + ", " + num1 + ".");
+			}
+			else if (num3 < num2 && num3 < num1 && num1 < num2) {
+				System.out.println("Ordem: " + num3 + ", " + num1 + ", " + num2 + ".");
+			}
+			else if (num3 < num2 && num3 < num1 && num2 < num1) {
+				System.out.println("Ordem: " + num3 + ", " + num2 + ", " + num1 + ".");
+			}
+				
+			
+		ler.close();
+	}
+
+}
