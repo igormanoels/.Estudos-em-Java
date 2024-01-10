b431c4cb99c669b4683f8aff2dc99dff6ed1b019
igormanoels <s.igormanoel@gmail.com>
2024-01-10 15:18:48 -0300

    Alterações nas pastas e inclusão de mais livros para estudos.

--- Curso Rocketseat/JavaIntrodution/src/Main.java
+++ Curso Rocketseat/JavaIntrodution/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         var nome = "fernanda";
 
-        System.out.print("Hello word" + nome);
+        System.out.print("Hello word" +  " " + nome);
     }
 }
 
