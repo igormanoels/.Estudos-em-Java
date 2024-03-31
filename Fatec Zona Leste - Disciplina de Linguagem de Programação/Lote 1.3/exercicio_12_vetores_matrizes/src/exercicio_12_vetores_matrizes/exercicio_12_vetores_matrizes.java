5d6fc85c3ff4f0ba11cbf560ff10f2a88fd1f70f
Igor Manoel <131622164+igormanoels@users.noreply.github.com>
2024-03-30 23:38:31 -0300

    Inclusão do exercício 12 do Lote 1.3 de LP

--- Fatec Zona Leste - Disciplina de Linguagem de Programação/Lote 1.3/exercicio_12_vetores_matrizes/src/exercicio_12_vetores_matrizes/exercicio_12_vetores_matrizes.java
+++ Fatec Zona Leste - Disciplina de Linguagem de Programação/Lote 1.3/exercicio_12_vetores_matrizes/src/exercicio_12_vetores_matrizes/exercicio_12_vetores_matrizes.java
@@ -17,6 +17,53 @@ public class exercicio_12_vetores_matrizes {
 
 	public static void main(String[] args) 
 	{
+		String xadrez[][] = new String[8][8];
 		
+		xadrez = geraXadrez(xadrez);
+		
+		exibeXadrez(xadrez);
+		
+	}
+	
+	
+	 /************************************************************
+     * Function que gera o tabuleiro de xadrez comforme a regra
+     */
+	static String[][] geraXadrez(String x[][])
+	{
+		int n = 0;
+		for (int i = 0; i < x.length; i++) 
+		{
+			for (int j = 0; j < x[i].length; j++) 
+			{
+				x[i][j] = (i == 1 || i == 6) ? "" + (int) (Math.pow(2, 0)) : " " ;
+				
+			}
+		}
+		
+		for (int i = 1; i <= 8; i++) 
+		{
+			n += (int) (Math.pow(i, i));
+		}
+		System.out.println("teste: "  + n);
+		
+		
+		return x;
+	}
+	
+	 /*******************************************
+     * Procedure que exibe os valores da matriz
+     */
+	static void exibeXadrez(String x[][]) 
+	{
+		System.out.println("Matriz: ");
+		for (int i = 0; i < x.length; i++) 
+		{
+			for (int j = 0; j < x[i].length; j++) 
+			{
+				System.out.print(x[i][j] + " " );
+			}
+			System.out.println(" ");
+		}
 	}
 }
