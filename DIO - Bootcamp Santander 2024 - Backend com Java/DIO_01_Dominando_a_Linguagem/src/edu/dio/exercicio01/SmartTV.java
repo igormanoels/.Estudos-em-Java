4d5782bb5077f50e40c95b243bea830c51590f60
igormanoels <s.igormanoel@gmail.com>
2024-06-04 23:59:46 -0300

    Estudos no bootcamp do Santander com a DIO - Segunda aula + exercicio

--- DIO - Bootcamp Santander 2024 - Backend com Java/DIO_01_Dominando_a_Linguagem/src/edu/dio/exercicio01/SmartTV.java
+++ DIO - Bootcamp Santander 2024 - Backend com Java/DIO_01_Dominando_a_Linguagem/src/edu/dio/exercicio01/SmartTV.java
@@ -14,6 +14,23 @@ public class SmartTV
 	int canal = 1;
 	int volume = 15;
 	
+	public void ligar()
+	{
+		ligada = true;
+	}
 	
+	public void desligar()
+	{
+		ligada = false;
+	}
 	
+	public void aumentarVolume()
+	{
+		volume++;
+	}
+	
+	public void diminuirVolume()
+	{
+		volume--;
+	}
 }
--- DIO - Bootcamp Santander 2024 - Backend com Java/DIO_01_Dominando_a_Linguagem/src/edu/dio/exercicio01/Usuario.java
+++ DIO - Bootcamp Santander 2024 - Backend com Java/DIO_01_Dominando_a_Linguagem/src/edu/dio/exercicio01/Usuario.java
@@ -9,5 +9,12 @@ public class Usuario
 		System.out.println("TV Lidaga: " + smartTv.ligada);
 		System.out.println("Canal Atual: " + smartTv.canal);
 		System.out.println("Volume Atual: " + smartTv.volume);
+		
+		smartTv.ligar();
+		System.out.println("TV Lidaga: " + smartTv.ligada);
+		
+		smartTv.desligar();
+		System.out.println("TV Lidaga: " + smartTv.ligada);
+		
 	}
 }
