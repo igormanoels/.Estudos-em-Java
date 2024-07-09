57c9ec2d101d5aa4cd283cf4e2adf2a7fa8a3202
igormanoels <s.igormanoel@gmail.com>
2024-07-09 00:58:47 -0300

    Algoritmo comentado, pois a IDE acusa erro de sobrecarga. Ainda que o código esteja correto e rodando.

--- DIO - Bootcamp Santander 2024 - Backend com Java/DIO_02_POO/src/edu/dio/exercicio11/Algoritmo04.java
+++ DIO - Bootcamp Santander 2024 - Backend com Java/DIO_02_POO/src/edu/dio/exercicio11/Algoritmo04.java
@@ -1,66 +1,66 @@
-package edu.dio.exercicio11;
-
-import java.text.DecimalFormat;
-import java.util.Scanner;
-
-public class Algoritmo04 
-{
-	/*
-	 * Orientação a Objetos na Prática com Java
-	 * 4 / 5 - Herança Bancária: Entendendo a Herança e Polimorfismo
-	 */
-    public static void main(String[] args) {
-        // Lendo os dados de Entrada:
-        Scanner scanner = new Scanner(System.in);
-        String titular = scanner.next();
-        int numeroConta = scanner.nextInt();
-        double saldo = scanner.nextDouble();
-        double taxaJuros = scanner.nextDouble();
-
-        ContaPoupanca contaPoupanca = new ContaPoupanca(numeroConta, titular, saldo, taxaJuros);
-
-        System.out.println("Conta Poupanca:");
-        contaPoupanca.exibirInformacoes();
-    }
-}
-
-
-// ESSE MÉTODO APRESENTA ERRO DE SOBRECARGA NA IMPRESSÃO, PORÉM ESTÁ CORRETO.
-class ContaBancaria 
-{
-    protected int numero;
-    protected String titular;
-    protected double saldo;
-
-    public ContaBancaria(int numero, String titular, double saldo) 
-    {
-        this.numero = numero;
-        this.titular = titular;
-        this.saldo = saldo;
-    }
-
-    public void exibirInformacoes() 
-    {
-        DecimalFormat decimalFormat = new DecimalFormat("#.0");
-        System.out.println(titular);
-        System.out.println(numero);
-        System.out.println("Saldo: R$ " + decimalFormat.format(saldo));
-    }
-}
-
-class ContaPoupanca extends ContaBancaria 
-{
-    private double taxaJuros;
-    
-    public ContaPoupanca(int numero, String titular, double saldo, double taxaJuros) 
-    {
-    	super(numero, titular, saldo);
-        this.taxaJuros = taxaJuros;
-    }
-
-    public void exibirInformacoes() 
-    {
-        super.exibirInformacoes();
-        System.out.println("Taxa de juros: " + taxaJuros + "%");
-    }
-}
+//package edu.dio.exercicio11;
+//
+//import java.text.DecimalFormat;
+//import java.util.Scanner;
+//
+//public class Algoritmo04 
+//{
+//	/*
+//	 * Orientação a Objetos na Prática com Java
+//	 * 4 / 5 - Herança Bancária: Entendendo a Herança e Polimorfismo
+//	 */
+//    public static void main(String[] args) {
+//        // Lendo os dados de Entrada:
+//        Scanner scanner = new Scanner(System.in);
+//        String titular = scanner.next();
+//        int numeroConta = scanner.nextInt();
+//        double saldo = scanner.nextDouble();
+//        double taxaJuros = scanner.nextDouble();
+//
+//        ContaPoupanca contaPoupanca = new ContaPoupanca(numeroConta, titular, saldo, taxaJuros);
+//
+//        System.out.println("Conta Poupanca:");
+//        contaPoupanca.exibirInformacoes();
+//    }
+//}
+//
+//
+//// ESSE MÉTODO APRESENTA ERRO DE SOBRECARGA NA IMPRESSÃO, PORÉM ESTÁ CORRETO.
+//class ContaBancaria 
+//{
+//    protected int numero;
+//    protected String titular;
+//    protected double saldo;
+//
+//    public ContaBancaria(int numero, String titular, double saldo) 
+//    {
+//        this.numero = numero;
+//        this.titular = titular;
+//        this.saldo = saldo;
+//    }
+//
+//    public void exibirInformacoes() 
+//    {
+//        DecimalFormat decimalFormat = new DecimalFormat("#.0");
+//        System.out.println(titular);
+//        System.out.println(numero);
+//        System.out.println("Saldo: R$ " + decimalFormat.format(saldo));
+//    }
+//}
+//
+//class ContaPoupanca extends ContaBancaria 
+//{
+//    private double taxaJuros;
+//    
+//    public ContaPoupanca(int numero, String titular, double saldo, double taxaJuros) 
+//    {
+//    	super(numero, titular, saldo);
+//        this.taxaJuros = taxaJuros;
+//    }
+//
+//    public void exibirInformacoes() 
+//    {
+//        super.exibirInformacoes();
+//        System.out.println("Taxa de juros: " + taxaJuros + "%");
+//    }
+//}
