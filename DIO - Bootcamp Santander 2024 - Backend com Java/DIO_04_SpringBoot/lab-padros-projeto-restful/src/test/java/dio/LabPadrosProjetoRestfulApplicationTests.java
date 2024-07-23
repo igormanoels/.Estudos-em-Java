520da03595996432c6aef6d90c841e5c5e0df057
igormanoels <s.igormanoel@gmail.com>
2024-07-23 20:04:44 -0300

    Continuação do Bootcamp Santander da DIO - Iniciando um projeto com RESTful API.

---
+++ DIO - Bootcamp Santander 2024 - Backend com Java/DIO_04_SpringBoot/lab-padros-projeto-restful/src/test/java/dio/LabPadrosProjetoRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadrosProjetoRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
