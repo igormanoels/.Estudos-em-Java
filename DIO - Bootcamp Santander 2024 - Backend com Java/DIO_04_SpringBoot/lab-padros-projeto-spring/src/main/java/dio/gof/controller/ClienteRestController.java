3e8924fa08e0320157388e3d8ab7b35d062196ea
igormanoels <s.igormanoel@gmail.com>
2024-07-23 14:16:41 -0300

    Continuação do Bootcamp Santander da DIO - Conclusão do projeto, trabalhando com Springboot, Projeto api consulta CEP.

--- DIO - Bootcamp Santander 2024 - Backend com Java/DIO_04_SpringBoot/lab-padros-projeto-spring/src/main/java/dio/gof/controller/ClienteRestController.java
+++ DIO - Bootcamp Santander 2024 - Backend com Java/DIO_04_SpringBoot/lab-padros-projeto-spring/src/main/java/dio/gof/controller/ClienteRestController.java
@@ -2,18 +2,13 @@ package dio.gof.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import dio.gof.model.Cliente;
 import dio.gof.service.ClienteService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
+@RequestMapping("clientes")
 public class ClienteRestController 
 {
 	@Autowired
@@ -42,7 +37,7 @@ public class ClienteRestController
 	}
 	
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Cliente> deletar(@PathVariable Long id){
+	public ResponseEntity<Void> deletar(@PathVariable Long id){
 		clienteService.deletar(id);
 		return ResponseEntity.ok().build();
 	}
--- DIO - Bootcamp Santander 2024 - Backend com Java/DIO_04_SpringBoot/lab-padros-projeto-spring/src/main/java/dio/gof/service/impl/ClienteServiceImpl.java
+++ DIO - Bootcamp Santander 2024 - Backend com Java/DIO_04_SpringBoot/lab-padros-projeto-spring/src/main/java/dio/gof/service/impl/ClienteServiceImpl.java
@@ -1,39 +1,76 @@
 package dio.gof.service.impl;
 
-import dio.gof.model.Cliente;
-import dio.gof.service.ClienteService;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dio.gof.model.Cliente;
+import dio.gof.model.ClienteRepository;
+import dio.gof.model.Endereco;
+import dio.gof.model.EnderecoRepository;
+import dio.gof.service.ClienteService;
+import dio.gof.service.ViaCepService;
+
+@Service
 public class ClienteServiceImpl implements ClienteService
 {
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private ViaCepService viaCepService;
+	
 	@Override
-	public Iterable<Cliente> buscarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+	public Iterable<Cliente> buscarTodos() 
+	{
+		return clienteRepository.findAll();
 	}
 
 	@Override
-	public Cliente buscarPorId(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Cliente buscarPorId(Long id) 
+	{
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		return cliente.get();
 	}
 
 	@Override
-	public void inserir(Cliente cliente) {
-		// TODO Auto-generated method stub
+	public void inserir(Cliente cliente) 
+	{
+		salvarClienteComCep(cliente);
+	}
+
+	// MÉTODO REUTILIZADO POR DOIS MÉTODOS
+	private void salvarClienteComCep(Cliente cliente) {
+		String cep = cliente.getEndereco().getCep();
+		Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+			Endereco novoEndereco = viaCepService.consultarCep(cep);
+			enderecoRepository.save(novoEndereco);
+			return novoEndereco;
+			});
+		cliente.setEndereco(endereco);
+		clienteRepository.save(cliente);
+	}
+
+	@Override
+	public void atualizar(Long id, Cliente cliente) 
+	{
+		Optional<Cliente> clienteBd = clienteRepository.findById(id);
+		if (clienteBd.isPresent())
+		{
+			salvarClienteComCep(cliente);
+		}
 		
 	}
 
 	@Override
-	public void atualizar(Long id, Cliente cliente) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deletar(Long id) {
-		// TODO Auto-generated method stub
-		
+	public void deletar(Long id) 
+	{
+		clienteRepository.deleteById(id);	
 	}
 	
 }
