6ad5ea1b1911afa5409a21d1e48bfeba4a4abd46
igormanoels <s.igormanoel@gmail.com>
2024-11-17 15:51:29 -0300

    Exclusão das aulas feitas sobre javafx.

--- Canal Youtube - Descompila - JavaFX para Iniciantes/Exemplo_Java_FX001/src/module-info.java
+++
@@ -1,5 +0,0 @@
-module Exemplo_Java_FX001 {
-	requires javafx.controls;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
--- Canal Youtube - Descompila - JavaFX para Iniciantes/Exemplo_Java_FX002/src/module-info.java
+++
@@ -1,5 +0,0 @@
-module Exemplo_Java_FX002 {
-	requires javafx.controls;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
