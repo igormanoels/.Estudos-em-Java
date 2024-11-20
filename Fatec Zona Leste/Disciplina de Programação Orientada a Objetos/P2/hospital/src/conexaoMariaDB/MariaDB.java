2db4ef437b97569a2632f468452f214621c5e6f2
igormanoels <s.igormanoel@gmail.com>
2024-11-19 23:58:00 -0300

    Continuação do trabalho da P2 de POO da graduação em ADS, implementando inserir especialidade.

---
+++ Fatec Zona Leste/Disciplina de Programação Orientada a Objetos/P2/hospital/src/conexaoMariaDB/MariaDB.java
@@ -0,0 +1,23 @@
+package conexaoMariaDB;
+
+public class MariaDB {
+
+//    private String URL = "jdbc:mariadb://localhost:3306/hospital?allowMultiQueries=true";
+    private String URL = "jdbc:mariadb://localhost:3306/hospital";
+    private String USER = "pooUser";
+    private String PASS = "123456";
+
+    public String getURL() {
+        return this.URL;
+    }
+
+    public String getUSER() {
+        return this.USER;
+    }
+
+    public String getPASS() {
+        return this.PASS;
+    }
+
+}
+
--- Fatec Zona Leste/Disciplina de Programação Orientada a Objetos/P2/hospital/src/igor/especialidade/especialidadeControl.java
+++ Fatec Zona Leste/Disciplina de Programação Orientada a Objetos/P2/hospital/src/igor/especialidade/especialidadeControl.java
@@ -3,30 +3,57 @@ package igor.especialidade;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import conexaoMariaDB.MariaDB;
 
 public class EspecialidadeControl {
-
-    final static String URL = "jdbc:mariadb://localhost:3306/agendadb?allowMultiQueries=true";
-    final static String USER = "root";
-    final static String PASS = "894523";
+    
+    private MariaDB mdb = new MariaDB();
+    private Connection con;
+    private EspecialidadeBoundary bd = new EspecialidadeBoundary();
 
     public void inserirEspecialidade() {
-
+            String sql = """
+                INSERT INTO especialidades (especialidade) 
+                VALUES (?)
+                    """;
+            conectarMariaDB();
+       try{     
+            PreparedStatement stm = this.con.prepareStatement(sql);
+            stm.setString(0, pegarEspecialidade());
+            stm.executeQuery();
+            con.close();
+       } catch (Exception e) {}
     }
 
-    public String buscarEspecialidade() {
-
+    public void buscarEspecialidade() {
+// CHAMAR A pegarEspecialidade PARA BUSCAR
     }
 
     public void alterarEspecialidade() {
-
+// CHAMAR A pegarEspecialidade PARA ALTERAR
     }
 
     public void removerEspecialidade() {
-        
+// CHAMAR A pegarEspecialidade PARA REMOVER
     }
 
+    private void conectarMariaDB() {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            this.con = DriverManager.getConnection(this.mdb.getURL(), this.mdb.getUSER(), this.mdb.getPASS());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String pegarEspecialidade() {
+        String esp = this.bd.txtEspecialidade.getText();
+        return esp;
+    }
+
+    private void inserirEspecialidade(String esp) {
+        this.bd.txtEspecialidade.setText(esp);
+    }
 
 
     
